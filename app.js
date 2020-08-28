d7523b89a5dcadcfef76d2198c2ca330dfc93f3e
bagus-arya <bagusarya850@gmail.com>
2020-08-28 19:35:54 +0700

    Fix css unload

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const express = require('express'),
       bodyParser = require('body-parser'),
       port = process.env.port||8000;
 const app = express();
+var path = require('path');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.static('public/public'));
-// app.use(express.static(path.join(__dirname + 'public')));
-// app.use('/public',express.static('/public'));
-// app.use(express.static(__dirname + 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //route untuk halaman home
 app.get('/',(req, res) => {
