203c6c4dc711310f80090a83c16bfc482ae7d288
bagus-arya <bagusarya850@gmail.com>
2020-08-28 12:45:37 +0700

    Update UI
    can't load CSS in folder public

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express'),
       connection = require('./database/config'),
-      bodyParser = require('body-parser')
+      bodyParser = require('body-parser'),
       port = process.env.port||8000;
-
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static('public/public'));
+// app.use(express.static(path.join(__dirname + 'public')));
+// app.use('/public',express.static('/public'));
+// app.use(express.static(__dirname + 'public'));
 
 //route untuk halaman home
 app.get('/',(req, res) => {
